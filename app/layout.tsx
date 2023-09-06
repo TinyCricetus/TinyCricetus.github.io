d8600d037ea1512b7591373a572bc3cb7c029eab
cricetus <tinycricetus@qq.com>
2023-09-06 13:56:22 +0800

    feat: 增加一个导航栏

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,20 @@ export default function RootLayout(config: LayoutConfig) {
   return (
     <html lang="en">
       <body>
-        {config.children}
+        <div className="main">
+          <div className="nav">
+            {
+              ['导航', '导航', '导航', '导航'].map(t => {
+                return (
+                  <div className="nav-item">{t}</div>
+                )
+              })
+            }
+          </div>
+          <div>
+            {config.children}
+          </div>
+        </div>
       </body>
     </html>
   )
